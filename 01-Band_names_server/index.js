03430b91d0674b745454ff62f1a182f6edd16b3f
Kike Sanchez <kikin1601@hotmail.com>
2021-02-04 10:34:56 -0500

    cliente conectado

--- 01-Band_names_server/index.js
+++ 01-Band_names_server/index.js
@@ -4,11 +4,23 @@ require('dotenv').config();
 
 const app = express();
 
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+// mensajes de sockets
+io.on('connection', client => {
+    console.log('Cliente conectado');
+
+    client.on('disconnect', () => {
+        console.log('Cliente desconectado!')
+    });
+});
+
 const publicPath = path.resolve(__dirname, 'public');
 
 app.use(express.static(publicPath));
 
-app.listen(process.env.PORT,(err)=>{
+server.listen(process.env.PORT,(err)=>{
     if(err) throw new Error(err);
     console.log('Servidor corriendo en puerto',process.env.PORT)
 })
