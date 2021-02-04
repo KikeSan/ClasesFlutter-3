45cdc3bfa8f3f8f2e5fa455fc393ab5e08ebc186
Kike Sanchez <kikin1601@hotmail.com>
2021-02-04 07:12:35 -0500

    public path

--- 01-Band_names_server/index.js
+++ 01-Band_names_server/index.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const path = require('path')
 const app = express();
 
-app.listen(300,(err)=>{
+const publicPath = path.resolve(__dirname, 'public');
+
+app.use(express.static(publicPath));
+
+app.listen(3000,(err)=>{
     if(err) throw new Error(err);
     console.log('Servidor corriendo en puerto',3000)
 })
